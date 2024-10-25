5463ce0c6bc6ddf36eb08a26fb606ec452c50245
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-10-25 00:27:29 -0400

    Creacion del contexto

---
+++ Proyecto_Final_Aplicada/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_Final_Aplicada.Models;
+
+namespace Proyecto_Final_Aplicada.DAL
+{
+    public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Taxistas> Taxistas { get; set; }
+
+    }
+}
