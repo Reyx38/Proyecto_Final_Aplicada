03015b820b694479d5d08ca6eda6ffa4c1a0e40b
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-10-25 00:00:04 -0400

    Creacion de las tres clases principales Clientes Taxistas Usuarios

---
+++ Proyecto_Final_Aplicada/Models/Clientes.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Final_Aplicada.Models
+{
+    public class Clientes : Usuario
+    {
+        [Key]
+        public int ClienteId { get; set; }
+    }
+}
