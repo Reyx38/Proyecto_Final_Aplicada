614da8db703da6ea788eb9c30f5a6e07e00163aa
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-03 12:27:15 -0400

    Se agrego el .Domain para agregar los Dto de los modelos y los enum que componen datos de algunos atributos en los modelos

---
+++ Proyecto_Final.Domain/Dto/TaxistaDto.cs
@@ -0,0 +1,21 @@
+﻿using Proyecto_Final.Domain.Enum;
+
+namespace Proyecto_Final.Domain.Dto
+{
+    public class TaxistaDto
+    {
+        public int TaxistaId { get; set; }
+
+        public string NickName { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Correo { get; set; }
+
+        public bool ExisteVehiculo { get; set; }
+
+        public bool ExisteLicencia { get; set; }
+
+        public EstadosTaxistas Status { get; set; }
+    }
+}
---
+++ Proyecto_Final.Domain/Enum/Provicias.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Proyecto_Final.Domain.Enum
+{
+    public enum Provicias
+    {
+        SantoDomingo,
+        DistritoNacional,
+        Santiago,
+        LaVega,
+        LaAltagracia,
+        SanCristobal,
+        SanPedroDeMacoris,
+        PuertoPlata,
+        Duarte,
+        Peravia
+    }
+}
