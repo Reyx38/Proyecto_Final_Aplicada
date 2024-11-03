75f36948eabb17e576d09b1d786dc319b93267c5
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-03 12:26:19 -0400

    Se creo que el proyecto de libreria . data para agregar todos los datos necesarios de los modelos, migraciones, el contexto, y la cadena de conexion

---
+++ Proyecto_Final.Data/Models/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Proyecto_Final.Domain.Enum;
+
+namespace Proyecto_Final.Data.Models
+{
+    public class Usuarios
+    {
+        public string NickName { get; set; }
+
+        [Required(ErrorMessage = "Los nombres son requerido.")]
+        [RegularExpression("^[a-zA-ZÀ-ÿ\\s]+$", ErrorMessage = "Solo se permiten letras.")]
+
+        public string Nombres { get; set; }
+        [Required(ErrorMessage = "La fecha de nacimiento es requerido.")]
+
+        public DateTime FechaNacimiento { get; set; }
+        [Required(ErrorMessage = "El Correo es requerido.")]
+
+        public string Correo { get; set; }
+        [Required(ErrorMessage = "La contraseña es requerido.")]
+
+        public string Password { get; set; }
+
+        public Provicias Provicia  { get; set; }
+
+        public string Role  { get; set; }
+    }
+}
