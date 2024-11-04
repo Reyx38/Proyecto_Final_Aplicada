ff8e1c5eb3afec63f953d6ee05c31ddebe7f45a1
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-04 16:14:23 -0400

    Taxista Service realizado

--- Proyecto_Final.Services/Services/TaxistaServices.cs
+++ Proyecto_Final.Services/Services/TaxistaServices.cs
@@ -1,36 +1,118 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Proyecto_Final.Abstracions.Interface;
 using Proyecto_Final.Data.Contexto;
+using Proyecto_Final.Data.Models;
 using Proyecto_Final.Domain.Dto;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace Proyecto_Final.Services.Services
 {
     public class TaxistaServices(IDbContextFactory<Context> DbFactory) : ITaxistaServices
     {
-        public Task<bool> Buscar(int id)
+        public async Task<TaxistaDto> Buscar(int id)
         {
-            throw new NotImplementedException();
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            var taxista = await contexto.Taxistas
+           .Where(e => e.TaxistaId == id)
+           .Select(p => new TaxistaDto()
+           {
+               TaxistaId = p.TaxistaId,
+               NickName = p.NickName,
+               Nombres = p.Nombres,
+               Correo = p.Correo,
+               ExisteVehiculo = p.ExisteVehiculo,
+               ExisteLicencia = p.ExisteLicencia,
+               Status = p.Status
+           })
+           .FirstOrDefaultAsync();
+            return taxista ?? new TaxistaDto();
         }
 
-        public Task<bool> Eliminar(TaxistaDto taxistaDto)
+        public async Task<bool> Eliminar(int id)
         {
-            throw new NotImplementedException();
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Taxistas
+                .Where(e => e.TaxistaId == id)
+                .ExecuteDeleteAsync() > 0;
         }
 
-        public Task<bool> ExisteTaxista(string nombre, int id, string correo, string provincia)
+        public async Task<bool> ExisteTaxista(string nombre, int id, string correo)
         {
-            throw new NotImplementedException();
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Taxistas
+                .AnyAsync(e => e.TaxistaId != id
+                && e.Nombres.ToLower().Equals(nombre.ToLower())
+                && e.Correo.ToLower().Equals(correo.ToLower()));
         }
 
-        public Task<bool> Guardar(TaxistaDto taxistaDto)
+        private async Task<bool> Insertar(TaxistaDto taxistaDto)
         {
-            throw new NotImplementedException();
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            var taxista = new Taxistas()
+            {
+                TaxistaId = taxistaDto.TaxistaId,
+                NickName = taxistaDto.NickName,
+                Nombres = taxistaDto.Nombres,
+                Correo = taxistaDto.Correo,
+                ExisteVehiculo = taxistaDto.ExisteVehiculo,
+                ExisteLicencia = taxistaDto.ExisteLicencia,
+                Status = taxistaDto.Status
+            };
+            contexto.Taxistas.Add(taxista);
+            var guardo = await contexto.SaveChangesAsync() > 0;
+            taxistaDto.TaxistaId = taxista.TaxistaId;
+            return guardo;
         }
 
-        public Task<bool> Listar(Expression<Func<TaxistaDto, bool>> criterio)
+        private async Task<bool> Modificar(TaxistaDto taxistaDto)
         {
-            throw new NotImplementedException();
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            var taxista = new Taxistas()
+            {
+                TaxistaId = taxistaDto.TaxistaId,
+                NickName = taxistaDto.NickName,
+                Nombres = taxistaDto.Nombres,
+                Correo = taxistaDto.Correo,
+                ExisteVehiculo = taxistaDto.ExisteVehiculo,
+                ExisteLicencia = taxistaDto.ExisteLicencia,
+                Status = taxistaDto.Status
+            };
+            contexto.Update(taxista);
+            var modificado = await contexto.SaveChangesAsync() > 0;
+            return modificado;
+        }
+
+        private async Task<bool> Existe(int id)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Taxistas
+                .AnyAsync(e => e.TaxistaId == id);
+        }
+
+        public async Task<bool> Guardar(TaxistaDto taxistaDto)
+        {
+            if (!await Existe(taxistaDto.TaxistaId))
+                return await Insertar(taxistaDto);
+            else
+                return await Modificar(taxistaDto);
+        }
+
+        public async Task<List<TaxistaDto>> Listar(Expression<Func<TaxistaDto, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Taxistas.Select(f => new TaxistaDto()
+            {
+                TaxistaId = f.TaxistaId,
+                NickName = f.NickName,
+                Nombres = f.Nombres,
+                Correo = f.Correo,
+                ExisteVehiculo = f.ExisteVehiculo,
+                ExisteLicencia = f.ExisteLicencia,
+                Status = f.Status
+            })
+            .Where(criterio)
+            .ToListAsync();
         }
     }
 }
