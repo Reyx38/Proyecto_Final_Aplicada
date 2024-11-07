d538209881fc43925aab1182f5740306c33b1549
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-07 09:37:01 -0400

    DTO de Viajes realizado

---
+++ Proyecto_Final.Domain/Dto/ViajesDto.cs
@@ -0,0 +1,27 @@
+﻿using Proyecto_Final.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final.Domain.Dto;
+
+public class ViajesDto
+{
+    public int ViajeId { get; set; }
+
+    public string UbicacionInicial { get; set; }
+
+    public string Destino { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public TimeOnly Tiempo { get; set; }
+
+    public EstadosViajes Estado { get; set; }
+
+    public int ClienteId { get; set; }
+
+    public int TaxistaId { get; set; }
+}
