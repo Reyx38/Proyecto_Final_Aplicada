09ab6877e4c8942a0005618f63e2e23689336797
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-08 00:07:44 -0400

    Enum para los tipos de Transacciones realizado

---
+++ Proyecto_Final.Domain/Enum/TipoTransaccion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final.Domain.Enum;
+
+public enum TipoTransaccion
+{
+    Recarga,
+    PagoViaje,
+    RecibirPago
+}
