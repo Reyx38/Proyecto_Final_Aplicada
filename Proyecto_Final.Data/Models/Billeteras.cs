bc16dfec159e7cb058bf013422ca2b11f1be7138
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-08 00:05:43 -0400

    Modelo Billeteras realizado

---
+++ Proyecto_Final.Data/Models/Billeteras.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final.Data.Models;
+
+public class Billeteras
+{
+    [Key]
+    public int BilleteraId { get; set; }
+
+    [Required]
+    public double Saldo { get; set; }
+
+    [ForeignKey("TransaccionId")]
+    public ICollection<Transacciones> Transacciones { get; set; } = new List<Transacciones>();
+}
