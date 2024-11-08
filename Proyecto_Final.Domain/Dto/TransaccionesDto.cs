2715562a8b34c45e35d0044b0d712cca2ded7b33
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-08 00:07:00 -0400

    DTO de Transacciones realizado

---
+++ Proyecto_Final.Domain/Dto/TransaccionesDto.cs
@@ -0,0 +1,16 @@
+﻿using Proyecto_Final.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final.Domain.Dto;
+
+public class TransaccionesDto
+{
+    public int TransaccionId { get; set; }
+    public DateTime Fecha { get; set; }
+    public double Monto { get; set; }
+    public TipoTransaccion Tipo { get; set; }
+}
