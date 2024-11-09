3196cccba474093de7e77640de6b3e6b0c345ec6
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-08 20:37:24 -0400

    Asociando una billetera a un cliente

--- Proyecto_Final.Data/Models/Clientes.cs
+++ Proyecto_Final.Data/Models/Clientes.cs
@@ -1,12 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Proyecto_Final.Data.Models
+namespace Proyecto_Final.Data.Models;
+
+public class Clientes : Usuarios
 {
-    public class Clientes : Usuarios
-    {
-        [Key]
-        public int ClienteId { get; set; }
+    [Key]
+    public int ClienteId { get; set; }
 
-        public ICollection<Taxistas> Favoritos { get; set; } = new List<Taxistas>();
-    }
+    public ICollection<Taxistas> Favoritos { get; set; } = new List<Taxistas>();
+
+    [ForeignKey("Billetera")]
+    public int BilleteraId { get; set; }
+    public Billeteras? Billetera { get; set; }
 }
