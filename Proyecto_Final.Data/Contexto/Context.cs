53d27d2b638e8000d13b43fcdbcda67be47e48c2
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-10 01:27:01 -0400

    Modificacion en el contexto para asignar valores correctos a los taxistas y  en su rol e estado, se creo una nueva migracion para agregar su modifciaciones

--- Proyecto_Final.Data/Contexto/Context.cs
+++ Proyecto_Final.Data/Contexto/Context.cs
@@ -38,7 +38,8 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
             FechaNacimiento = new DateTime(1985, 4, 15),
             Correo = "federicoLiranzo01@gmail.com",
             Password = "elcompa1985",
-            Role = "Taxista",
+            Role = Roles.Empleado,
+            Status = EstadosTaxistas.Disponible,
             BilleteraId = 1
             },
         new Taxistas()
@@ -50,8 +51,9 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
             FechaNacimiento = new DateTime(1995, 7, 25),
             Correo = "cesarpolanco1@gmail.com",
             Password = "sorollo1995",
-            Role = "Taxista",
-            BilleteraId = 2
+			Role = Roles.Empleado,
+			Status = EstadosTaxistas.Disponible,
+			BilleteraId = 2
         },
         new Taxistas()
         {
@@ -62,8 +64,9 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
             FechaNacimiento = new DateTime(1990, 12, 5),
             Correo = "martinperez90@gmail.com",
             Password = "eltino1990",
-            Role = "Taxista",
-            BilleteraId = 3
+			Role = Roles.Empleado,
+			Status = EstadosTaxistas.Disponible,
+			BilleteraId = 3
         },
         new Taxistas()
         {
@@ -74,8 +77,9 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
             FechaNacimiento = new DateTime(1987, 3, 17),
             Correo = "juanrodriguez87@gmail.com",
             Password = "rapido1987",
-            Role = "Taxista",
-            BilleteraId = 4
+            Status = EstadosTaxistas.Disponible,
+			Role = Roles.Empleado,
+			BilleteraId = 4
         },
         new Taxistas()
         {
@@ -86,10 +90,12 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
             FechaNacimiento = new DateTime(1992, 8, 30),
             Correo = "carlosherrera92@gmail.com",
             Password = "chico1992",
-            Role = "Taxista"
+			Status = EstadosTaxistas.Disponible,
+			Role = Roles.Empleado,
+			BilleteraId = 5
 
-        }
-    });
+		}
+	});
         modelBuilder.Entity<Billeteras>().HasData(new List<Billeteras>()
         {
             new Billeteras() { BilleteraId = 1, Saldo = 450 },
