7a2437957868addc3431ac7053a0601c149fb860
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-27 11:28:26 -0400

    Creacion del modelo ViajesRapidos

---
+++ Proyecto_Final.Data/Models/ViajesRapidos.cs
@@ -0,0 +1,44 @@
+﻿using ReyAI_Trasport.Data.Models;
+using ReyAI_Trasport.Data;
+using ReyAI_Trasport.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReyAI_Trasport.Data.Models;
+
+public class ViajesRapidos
+{
+    [Key]
+    public int ViajeRapidoId { get; set; }
+
+    [ForeignKey("DestinoCerca")]
+    public int DestinoCercaId { get; set; }
+    public DestinosCerca? DestinoCerca { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public DateTime Fecha { get; set; }
+
+    [ForeignKey("EstadoViaje")]
+    public int EstadoVId { get; set; }
+    public EstadosViajes? EstadoViaje { get; set; }
+
+    [Required]
+    public double Precio { get; set; }
+
+    [Required]
+    public int personas { get; set; }
+
+
+    [ForeignKey("Taxista")]
+    public string? TaxistaId { get; set; }
+    public ApplicationUser? Taxista { get; set; }
+
+    [ForeignKey("Cliente")]
+    public string? ClienteId { get; set; }
+    public ApplicationUser? Cliente { get; set; }
+}
---
+++ Proyecto_Final.Domain/Dto/ViajesRapidosDto.cs
@@ -0,0 +1,27 @@
+﻿using ReyAI_Trasport.Domain.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReyAI_Trasport.Domain.Dto;
+
+public class ViajesRapidosDto
+{
+    public int ViajeRapidoId { get; set; }
+
+    public int DestinoCercaId { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public int EstadoVId { get; set; }
+
+    public double Precio { get; set; }
+
+    public int personas { get; set; }
+
+    public string ClienteId { get; set; }
+
+    public string TaxistaId { get; set; }
+}
