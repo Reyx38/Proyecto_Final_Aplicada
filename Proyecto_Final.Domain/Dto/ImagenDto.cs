2f9026677bb14a0879cab9594cc5b7cd9bcbb033
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-27 02:26:10 -0400

    modificacion en la imagenDto y continuacion del create-Viajes

--- Proyecto_Final.Domain/Dto/ImagenDto.cs
+++ Proyecto_Final.Domain/Dto/ImagenDto.cs
@@ -11,6 +11,7 @@ public class ImagenDto
     public int Id { get; set; }
     public string ImagenUrl { get; set; }
     public string Alt { get; set; }
-    public string Titulo { get; set; }
-    public int ViajesId { get; set; }
+    public string? Base64 { get; set; }
+	public string? Titulo { get; set; }
+	public int ViajesId { get; set; }
 }
