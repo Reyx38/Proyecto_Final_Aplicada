9148312aef4b0188879836cbef2c725bc10a3bc4
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-28 15:53:42 -0400

    Agregando detalles

--- Proyecto_Final.Domain/Dto/ReservacionesDto.cs
+++ Proyecto_Final.Domain/Dto/ReservacionesDto.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ReyAI_Trasport.Domain.Dto;
 
@@ -17,6 +18,7 @@ public class ReservacionesDto
     public int ViajeId { get; set; }
 
     public bool Pago { get; set; }
-
     public string? Recibo { get; set; }
+
+    public ICollection<ReservacionDetallesDto> ReservacionDetalles { get; set; } = new List<ReservacionDetallesDto>();
 }
