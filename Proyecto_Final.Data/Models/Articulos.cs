c05ff44c88b393b59a2df0d53fba0549b469d106
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-28 15:27:31 -0400

    Creando Articulos

---
+++ Proyecto_Final.Data/Models/Articulos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReyAI_Trasport.Data.Models;
+
+public class Articulos
+{
+    [Key]
+    public int ArticuloId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten numeros enteros o decimales")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public double Costo { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public double Precio { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public int Existencia { get; set; }
+}
