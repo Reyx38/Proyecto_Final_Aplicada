d8e23db10ae0cb461db61a2050cc79dd7813d997
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-29 13:39:01 -0400

    Creando modelo Pagos y su DTO

---
+++ Proyecto_Final.Data/Models/Pagos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReyAI_Trasport.Data.Models;
+
+public class Pagos
+{
+    [Key]
+    public int PagoId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public double Monto { get; set;}
+
+    [ForeignKey("MetodoPago")]
+    public int MetodoPId { get; set; }
+    public MetodosPagos metodoPago { get; set; }
+
+    [ForeignKey("Reservacion")]
+    public int ReservacionId { get; set; }
+    public Reservaciones Reservacion { get; set; }
+}
