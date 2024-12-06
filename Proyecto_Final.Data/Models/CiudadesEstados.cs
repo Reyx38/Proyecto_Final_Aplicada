f5955d4a256994a13635e52809130c8d09105688
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-12-06 01:46:50 -0400

    CAMBIOS EN LOS MODELOS PARA MANEJAR LOS DATOS DE LOS NUEVOS ESTADOS

---
+++ Proyecto_Final.Data/Models/CiudadesEstados.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Final.Data.Models
+{
+    public class CiudadesEstados
+    {
+        [Key]
+        public int EstadosCId { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [RegularExpression("^[a-zA-ZÀ-ÿ\\s]+$", ErrorMessage = "Solo se permiten letras.")]
+        public string? Descripcion { get; set; }
+    }
+}
