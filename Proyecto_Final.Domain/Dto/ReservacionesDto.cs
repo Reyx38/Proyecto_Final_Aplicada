66148b59336b29a6e8a720f93b4a3b8659857f77
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-12-05 21:53:58 -0400

    Validaciones agregadas a recervaciones

--- Proyecto_Final.Domain/Dto/ReservacionesDto.cs
+++ Proyecto_Final.Domain/Dto/ReservacionesDto.cs
@@ -13,21 +13,27 @@ public class ReservacionesDto
 {
     public int ReservacionId { get; set; }
 
+    [Required(ErrorMessage = "Campo obligatorio")]
     public DateTime Fecha { get; set; }
 
+    [Required(ErrorMessage = "Campo obligatorio")]
     public int ViajeId { get; set; }
     public string? ViajeDto { get; set; }
 
     public bool Pago { get; set; }
     public string? Recibo { get; set; }
 
-	public double Monto { get; set; }
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public double Monto { get; set; }
 
+    [Required(ErrorMessage = "Campo obligatorio")]
     public int EstadoId { get; set; }
     public  string? EstadosReservacionesDto { get; set; }
 
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string? ClienteId { get; set; }
 
+    [Required(ErrorMessage = "Campo obligatorio")]
     public int CantidadPasajeros { get; set; }
 
 	public ICollection<ReservacionDetallesDto> ReservacionDetalles { get; set; } = new List<ReservacionDetallesDto>();
